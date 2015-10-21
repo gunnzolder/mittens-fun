0f699ba1696269cde1db1a818285b29619e4e6b1
Anton Parkhomenko <anton380@gmail.com>
2015-10-21 03:14:39 +0300

    minor rafactoring

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 
     var symbols = document.querySelectorAll('symbol'),
         use,
@@ -9,21 +9,20 @@
         x, y;
 
     function setAttributes(el, id, color, x, y) {
-
-                el.setAttributeNS(xlinkns, 'xlink:href', '#'+id);
-                el.setAttributeNS(null, 'style', 'fill:'+color);
-                el.setAttributeNS(null, 'x', x);
-                el.setAttributeNS(null, 'y', y);
-                el.setAttributeNS(null, 'width', 80);
-                el.setAttributeNS(null, 'height', 80);
-                //el.setAttributeNS(null, 'transform', 'rotate('+deg+')');
+        el.setAttributeNS(xlinkns, 'xlink:href', '#' + id);
+        el.setAttributeNS(null, 'style', 'fill:' + color);
+        el.setAttributeNS(null, 'x', x);
+        el.setAttributeNS(null, 'y', y);
+        el.setAttributeNS(null, 'width', 80);
+        el.setAttributeNS(null, 'height', 80);
+        //el.setAttributeNS(null, 'transform', 'rotate('+deg+')');
 
     }
 
     function getRandomColor() {
         var letters = '0123456789ABCDEF'.split('');
         var color = '#';
-        for (var i = 0; i < 6; i++ ) {
+        for (var i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
@@ -36,19 +35,14 @@
         size: 80
     };
 
-    function generateMittens(gridX,gridY,gridSize) {
-        svg = document.querySelector('svg');
-        console.log(svg);
+    function generateMittens(gridX, gridY, gridSize) {
 
-        for (x=0; x < gridX * gridSize; x += gridSize) {
-            for (y=0; y < gridY * gridSize; y += gridSize) {
+        for (x = 0; x < gridX * gridSize; x += gridSize) {
+            for (y = 0; y < gridY * gridSize; y += gridSize) {
 
                 use = document.createElementNS("http://www.w3.org/2000/svg", 'use');
-
                 mitten = Math.floor(Math.random() * symbols.length);
-
                 setAttributes(use, symbols[mitten].id, getRandomColor(), x, y);
-
                 svg.appendChild(use);
 
             }
@@ -56,15 +50,15 @@
     }
 
     var button = document.querySelector('button');
-    button.onclick = function(){
+    button.onclick = function () {
         var uses = document.querySelectorAll('use'), i;
         for (i = 0; i < uses.length; ++i) {
             svg.removeChild(uses[i]);
         }
-        generateMittens(grid.x,grid.y,grid.size);
+        generateMittens(grid.x, grid.y, grid.size);
     };
 
-    generateMittens(grid.x,grid.y,grid.size);
+    generateMittens(grid.x, grid.y, grid.size);
 
 
 })();
